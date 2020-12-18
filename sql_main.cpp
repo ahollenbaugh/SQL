cf62f571d183a77cd2939b94ddb38233f4075a2e
ahollenbaugh <56135023+ahollenbaugh@users.noreply.github.com>
2020-12-18 15:38:12 -0800

    Add files via upload

---
+++ sql_main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "../../!includes/SQL/sql.h"
+using namespace std;
+
+int main()
+{
+    SQL program;
+    cout << endl << endl << "-------------------- PROGRAM START --------------------" << endl << endl;
+    program.run();
+
+    return 0;
+}
